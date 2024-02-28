23fbcd66171bcdbd5359e240b9ff4e188976a0e2
Cesar Roman <cesardroman18@gmail.com>
2024-02-28 10:18:18 -0600

    added local keymaps to chat gippity

--- lua/plugins/chatgpt-nvim.lua
+++ lua/plugins/chatgpt-nvim.lua
@@ -8,6 +8,21 @@ return {
 		"nvim-telescope/telescope.nvim",
 	},
 	config = function()
-		require("chatgpt").setup()
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>ch", ":ChatGPT<CR>", { desc = "ChatGippity" })
+		keymap.set("n", "<leader>cha", ":ChatGPTActAs<CR>", { desc = "ChatGippity [a]ct as" })
+		keymap.set("n", "<leader>chc", ":ChatGPTCompleteCode<CR>", { desc = "ChatGippity [c]omplete code" })
+		keymap.set(
+			"n",
+			"<leader>che",
+			":ChatGPTEditWithInstructions<CR>",
+			{ desc = "ChatGippity [e]dit with instructions" }
+		)
+		keymap.set("n", "<leader>chr", ":ChatGPTRun", { desc = "ChatGippity" })
+
+		require("chatgpt").setup({
+			api_key_cmd = "echo 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",
+		})
 	end,
 }
