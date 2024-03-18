f18de4bef0394987fda7c9de5978cbff1bab02d0
Cesar Roman <cesardroman18@gmail.com>
2024-03-17 22:14:32 -0500

    feat: added options for scrolloff

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -49,3 +49,8 @@ opt.foldcolumn = "0"
 opt.foldlevel = 99
 opt.foldlevelstart = 99
 opt.foldenable = true
+--
+-- Case-insensitive searching UNLESS \C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+opt.scrolloff = 8
