9e33dcac58dc77e3d3cf7283989298fcd172a8d0
cesartheroman <cesardroman18@gmail.com>
2024-12-24 17:47:01 -0600

    [refactor] change way to navigate hunks based on API change

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -12,16 +12,21 @@ return {
         },
         on_attach = function(bufnr)
             local builtin = require('telescope.builtin')
+            local gitsigns = require('gitsigns')
 
             vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { desc = '[G]it Status' })
             vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })
-            vim.keymap.set('n', '<leader>gl', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]it [L]ast Hunk' })
-            vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]it [N]ext Hunk' })
-            vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[G]it [P]review Hunk' })
-            vim.keymap.set('n', '<leader>gb', require('gitsigns').toggle_current_line_blame, { buffer = bufnr, desc = '[G]it [B]lame' })
-            vim.keymap.set('n', '<leader>gst', require('gitsigns').stage_hunk, { desc = '[G]it [S]tage' })
-            vim.keymap.set('n', '<leader>gsu', require('gitsigns').undo_stage_hunk, { desc = '[G]it [S]tage [U]ndo' })
-            vim.keymap.set('n', '<leader>gsb', require('gitsigns').stage_buffer, { desc = '[G]it [S]tage [B]uffer' })
+            vim.keymap.set('n', '<leader>gl', function()
+                gitsigns.nav_hunk('prev')
+            end, { buffer = bufnr, desc = '[G]it [L]ast Hunk' })
+            vim.keymap.set('n', '<leader>gn', function()
+                gitsigns.nav_hunk('next')
+            end, { buffer = bufnr, desc = '[G]it [N]ext Hunk' })
+            vim.keymap.set('n', '<leader>gp', gitsigns.preview_hunk, { buffer = bufnr, desc = '[G]it [P]review Hunk' })
+            vim.keymap.set('n', '<leader>gb', gitsigns.toggle_current_line_blame, { buffer = bufnr, desc = '[G]it [B]lame' })
+            vim.keymap.set('n', '<leader>gsh', gitsigns.stage_hunk, { desc = '[G]it [S]tage [H]unk' })
+            vim.keymap.set('n', '<leader>gsu', gitsigns.undo_stage_hunk, { desc = '[G]it [S]tage [U]ndo' })
+            vim.keymap.set('n', '<leader>gsb', gitsigns.stage_buffer, { desc = '[G]it [S]tage [B]uffer' })
         end,
     },
 }
