a110560c51b0a3fd75da60e50cb8e1f3bb9817fd
cesartheroman <cesardroman18@gmail.com>
2025-05-03 12:42:17 -0500

    fix: diagnostics virtual text

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -123,6 +123,15 @@ return {
             end,
         })
 
+        vim.diagnostic.config({
+            virtual_text = {
+                enable = true,
+                -- prefix = '●',
+                -- spacing = 2,
+            },
+            severity_sort = true, -- Sort diagnostics by severity
+        })
+
         -- LSP servers and clients are able to communicate to each other what features they support.
         --  By default, Neovim doesn't support everything that is in the LSP specification.
         --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
