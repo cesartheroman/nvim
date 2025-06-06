2e27b4e25f60b38c7a7139c5d21bd5168b9bccc2
cesartheroman <cesardroman18@gmail.com>
2025-06-06 00:05:00 -0500

    finally figured out how to load from env!

--- lua/plugins/chatgpt-nvim.lua
+++ lua/plugins/chatgpt-nvim.lua
@@ -12,15 +12,25 @@ return {
         { 'nvim-telescope/telescope.nvim', cmd = 'Telescope' },
     },
     config = function()
+        local api_key = os.getenv('OPEN_ROUTER_API_KEY')
+        if not api_key then
+            vim.notify(
+                'Error: OPEN_ROUTER_API_KEY env var is not set.' .. 'ChatGippity.nvim wil not function correctly.',
+                vim.log.levels.ERROR,
+                { title = 'ChatGippity.nvim Config Error' }
+            )
+        end
         require('chatgpt').setup({
             api_host_cmd = 'echo -n https://openrouter.ai/api',
-            api_key_cmd = 'op read op://Personal/open-router/api-key',
+            api_key_cmd = 'echo -n ' .. api_key,
             api_type_cmd = 'echo localai',
             openai_params = {
                 model = 'google/gemini-2.5-flash-preview-05-20',
+                -- model = 'anthropic/claude-sonnet-4',
                 -- model = 'google/gemini-2.5-pro-preview',
                 -- model = 'openai/o4-mini',
-                max_tokens = 2048,
+                -- model = 'openrouter/auto',
+                max_tokens = 2000,
             },
             extra_curl_params = {
                 '-H',
